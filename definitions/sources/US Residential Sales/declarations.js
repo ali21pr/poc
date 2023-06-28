c4c27ba05abdb77da6bb7f1ebf02bc4aa7e0699e
Ali Boudjemai <ali.boudjemai@keyrus.com>
2023-06-28 07:49:28 +0000

    add

---
+++ definitions/sources/US Residential Sales/declarations.js
@@ -0,0 +1,5 @@
+declare({
+    database: constants.NT_DATABASE,
+    schema: constants.NT_DATASET,
+    name: constants.NT_TABLE,
+});
---
+++ includes/constants.js
@@ -0,0 +1,4 @@
+const NT_DATABASE = 'kyr-sandbox-test-abo';
+const NT_DATASET = 'kyr_sandbox_test_abo_bqd_my_dataset';
+const NT_TABLE = 'national_trends';
+module.exports = {NT_DATABASE, NT_DATASET, NT_TABLE}
