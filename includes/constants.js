0b74eef2791c7580463ec9546f20329fac331e34
Ali Boudjemai <ali.boudjemai@keyrus.com>
2023-07-03 08:53:18 +0000

    add

--- includes/constants.js
+++ includes/constants.js
@@ -1,4 +1,25 @@
 const NT_DATABASE = 'kyr-sandbox-test-abo';
-const NT_DATASET = 'kyr_sandbox_test_abo_bqd_my_dataset';
-const NT_TABLE = 'national_trends';
-module.exports = {NT_DATABASE, NT_DATASET, NT_TABLE}
+const NT_DATASET = 'kyr_sandbox_test_abo_bqd_Dataset_dataform';
+const BRV_TABLE = 'block_rental_value';
+
+const BV_TABLE = 'block_value'; 
+
+const BVT_TABLE = 'block_value_ts'; 
+
+const MS_TABLE = 'msa_cs'; 
+
+const MST_TABLE = 'msa_ts'; 
+
+const NC_TABLE = 'national_cs'; 
+
+const NT_TABLE = 'national_ts'; 
+
+const ST_TABLE = 'state_ts'; 
+
+const ZC_TABLE = 'zip_cs'; 
+
+const ZT_TABLE = 'zip_ts'; 
+
+module.exports = {NT_DATABASE, NT_DATASET, BRV_TABLE, BV_TABLE, BVT_TABLE, MS_TABLE, MST_TABLE, NC_TABLE,
+NT_TABLE, ST_TABLE, ZC_TABLE, ZT_TABLE}
+
