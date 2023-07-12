18a2f60f7551f39f9810ca48f88beb6a507266d6
ali.boudjemai@keyrus.com <ali.boudjemai@keyrus.com>
2023-07-12 13:53:30 +0000

    add

--- definitions/sources/US Residential Sales/declarations.js
+++ definitions/sources/US Residential Sales/declarations.js
@@ -1,59 +1,59 @@
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.BRV_TABLE,
 });
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.BVT_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.MS_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.MST_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.NC_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.NT_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.ST_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.ZC_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.ZT_TABLE,
 });
 
 declare({
-    database: constants.NT_DATABASE,
-    schema: constants.NT_DATASET,
+    database: constants.URR_DATABASE,
+    schema: constants.URR_DATASET,
     name: constants.BV_TABLE,
 });
 
--- includes/constants.js
+++ includes/constants.js
@@ -1,6 +1,7 @@
-const NT_DATABASE = 'kyr-sandbox-test-abo';
-const NT_DATASET = 'kyr_sandbox_test_abo_bqd_Dataset_dataform';
+const URR_DATABASE = 'kyr-sandbox-test-abo';
+const URR_DATASET = 'kyr_sandbox_test_abo_bqd_Dataset_dataform';
 const BRV_TABLE = 'block_rental_value';
+
 
 const BV_TABLE = 'block_value'; 
 
@@ -20,6 +21,5 @@ const ZC_TABLE = 'zip_cs';
 
 const ZT_TABLE = 'zip_ts'; 
 
-module.exports = {NT_DATABASE, NT_DATASET, BRV_TABLE, BV_TABLE, BVT_TABLE, MS_TABLE, MST_TABLE, NC_TABLE,
-NT_TABLE, ST_TABLE, ZC_TABLE, ZT_TABLE}
+module.exports = { URR_DATABASE, URR_DATASET, BRV_TABLE, BV_TABLE, BVT_TABLE, MS_TABLE, MST_TABLE, NC_TABLE, NT_TABLE, ST_TABLE, ZC_TABLE, ZT_TABLE}
 
